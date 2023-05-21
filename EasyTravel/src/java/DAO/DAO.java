7fe1178fd627c94877bb811504196c91ae6ab9ca
DUC <tranminhducbxvp@gmail.com>
2023-05-21 18:54:55 +0700

    Project structure - Iteration 1

---
+++ EasyTravel/src/java/DAO/DAO.java
@@ -0,0 +1,33 @@
+
+/*
+ * ISP392-IS1701-Group6
+ * EasyTravel
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * 21-05-2023      1.0                 DucTM           First Implement
+ */
+package DAO;
+
+import java.util.List;
+
+/*
+ * This interface contains basic methods for performing CRUD actions from/to the database
+ * All DAO classess will implement this interface
+ * 
+ * 
+ * @author DucTM 
+ * @param <T> class of the object the implementing DAO will handle
+ */
+public interface DAO<T> {
+
+    List<T> getAll();
+
+    T get(int id);
+
+    void save(T t);
+
+    void update(T t);
+
+    void delete(T t);
+}
