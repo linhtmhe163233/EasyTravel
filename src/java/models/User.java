4cef3120afd1cf237df23e14e6f4e22cfb32c830
My Laptop <My Laptop@DESKTOP-AUBIF4K>
2023-05-27 19:59:30 +0700

    User variable declaration. Implement user.java.

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -2,5 +2,88 @@
 package models;
 
 public class User {
+    private String userame;
+    private String password;
+    private String fullname;
+    private String email;
+    private String phonenumber;
+    private int role;
+    private int dob;
+
+    public User() {
+    }
+
+    public User(String userame, String password, String fullname, String email, String phonenumber, int role, int dob) {
+        this.userame = userame;
+        this.password = password;
+        this.fullname = fullname;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.role = role;
+        this.dob = dob;
+    }
+
+    public String getUserame() {
+        return userame;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setUserame(String userame) {
+        this.userame = userame;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userame=" + userame + ", password=" + password + ", fullname=" + fullname + ", email=" + email + ", phonenumber=" + phonenumber + ", role=" + role + ", dob=" + dob + '}';
+    }
+   
+     
 
 }
