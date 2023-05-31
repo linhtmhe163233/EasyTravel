1d2f0a5adb85bd8c60eba73c8dcf56327596da65
DUC <tranminhducbxvp@gmail.com>
2023-05-31 15:28:37 +0700

    add date constraints

--- web/js/AddNewTour.js
+++ web/js/AddNewTour.js
@@ -5,22 +5,32 @@
  * Record of change:
  * DATE            Version             AUTHOR           DESCRIPTION
  * 29-05-2023      1.0                 DucTM           First Implement
+ * 31-05-2023      1.0                 DucTM           Add date constraints
  */
 let dateNow = new Date();
-document.getElementById("available_from").valueAsDate = dateNow;
-document.getElementById("available_to").valueAsDate = dateNow;
+let from = document.getElementById("available_from");
+let to = document.getElementById("available_to");
 
-document.getElementById("available_from").min=dateNow.toLocaleDateString();
-document.getElementById("available_from").max=addDays(dateNow, 365);
+Date.prototype.addDays = function (days) {
+    let date = new Date(this);
+    date.setDate(date.getDate() + days);
+    return date;
+};
 
+from.onchange = () => {
+    to.min = from.valueAsDate.addDays(1).toISOString().split("T")[0];
+    to.valueAsDate = from.valueAsDate.addDays(1);
+};
 
+document.getElementById("available_from").min = dateNow.toISOString().split("T")[0];
+from.valueAsDate = dateNow;
+to.valueAsDate = dateNow.addDays(1);
+
+let image=document.getElementById("image");
+let imageDisplay=document.getElementById("imageDisplay");
 image.onchange = () => {
     const file = image.files[0];
     if (file) {
-        imageDisplay.src = URL.createObjectURL(file)
+        imageDisplay.src = URL.createObjectURL(file);
     }
-}
-function addDays(date, days) {
-  date.setDate(date.getDate() + days);
-  return date;
-}
+};
